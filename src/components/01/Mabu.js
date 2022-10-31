a92f80c04b27f78ae8acc8e477421cd21a9da134
mahaboob jani <skmabujani521@gmail.com>
2022-10-31 18:37:24 +0530

    mahaboobjani

---
+++ src/components/01/Mabu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import '../components/Mabu'
+import './Mabu.css'
+function Mabu()
+{
+    return(
+        <div className='hello'>
+  <h1>About us</h1>
+  <p>At Zappos.com our purpuse is simple: to live and deliver WOW</p>
+  <p>Twenty years ago we began as a small online retailer that only sold shoes. Today we still sell shoes- as well as</p>
+  <p>clothing, handbags, accessories and more.that more is providing the very best customer service.customer</p>
+  <p>experiance and company culture we aim to inspire the world by showing its possible to simultaneously deliver</p>
+  <p>happiness to customers employees vendours shareholders and the community in a logn term sustainable way</p>
+
+  <p>we hope that in the future people wont even realize we started selling shoes online instead they know Zappos as</p>
+  <p>service comapny that just happens to sell___</p>
+        </div>
+    )
+}
+export default Mabu
